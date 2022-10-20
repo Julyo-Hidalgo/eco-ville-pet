7f65db79178b3c62e32753e1adc7145fd2cfced1
Julyo Hidalgo <julyohidalgo0@gmail.com>
2022-10-19 21:20:46 -0300

    atualizando página adquirir

--- js/funcoes.js
+++ js/funcoes.js
@@ -7,3 +7,9 @@ function fecha_menu() {
     document.getElementById("btn_fechar").tabindex = "1";
     document.getElementById("drop_link").style.display = "none";
 }
+
+function calcular_total(){
+    var qnt = 
+
+    document.getElementById("cTotal");
+}
